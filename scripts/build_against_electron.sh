dea0eaad1c6bd4482f1655dd2c9668c224c44f10
tsufeki <tsufeki@ymail.com>
2017-03-21 22:44:31 +0100

    Install electron with -g

--- scripts/build_against_electron.sh
+++ scripts/build_against_electron.sh
@@ -2,50 +2,40 @@
 
 source ~/.nvm/nvm.sh
 
-set -e -u -x
+set -e -u
 
-GYP_ARGS="--runtime=electron --target=${ELECTRON_VERSION} --dist-url=https://atom.io/download/electron"
 export DISPLAY=":99.0"
+GYP_ARGS="--runtime=electron --target=${ELECTRON_VERSION} --dist-url=https://atom.io/download/electron"
+NPM_BIN_DIR="$(npm bin -g 2>/dev/null)"
+echo $NPM_BIN_DIR
 
 function publish() {
     if [[ ${PUBLISHABLE:-false} == true ]] && [[ ${COMMIT_MESSAGE} =~ "[publish binary]" ]]; then
         node-pre-gyp package
         node-pre-gyp publish
         node-pre-gyp info
-        make clean
     fi
 }
 
 function electron_pretest() {
-    npm install electron
-    echo $?
-    npm install electron-mocha
-    echo $?
+    npm install -g electron
+    npm install -g electron-mocha
     sh -e /etc/init.d/xvfb start
-    echo $?
     sleep 3
 }
 
 function electron_test() {
-    echo $(npm bin)
-    ls -l $(npm bin)
-    echo "var {app} = require('electron'); require('./createdb.js')(function () { app.quit(); });" >test/support/createdb-electron.js
-    ls -l test/support/
-    "$(npm bin)"/electron test/support/createdb-electron.js
-    rm test/support/createdb-electron.js
-    "$(npm bin)"/electron-mocha -R spec --timeout 480000
+    "$NPM_BIN_DIR"/electron test/support/createdb-electron.js
+    "$NPM_BIN_DIR"/electron-mocha -R spec --timeout 480000
 }
 
 # test installing from source
 npm install --build-from-source  --clang=1 $GYP_ARGS
-echo $?
 electron_pretest
-echo $?
 electron_test
-echo $?
-
 
 publish
+make clean
 
 # now test building against shared sqlite
 export NODE_SQLITE3_JSON1=no
